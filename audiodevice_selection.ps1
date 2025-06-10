937720552092d09558c6d26ca7d8c64ff7e8ee70
git4gw <105308799+git4gw@users.noreply.github.com>
2025-06-10 20:25:00 +0200

    Update audiodevice_selection.ps1
    Inproved Version

--- audiodevice_selection.ps1
+++ audiodevice_selection.ps1
@@ -1,3 +1,12 @@
+#
+# audiodevice selection with powershell menu
+# Ed. 01
+#
+# (c) G. Wenz
+#
+# 2025-06-10
+#
+
 # Ensure that all country dependent characters are shown correctly 
 
 [Console]::InputEncoding  = [System.Text.Encoding]::UTF8
@@ -13,15 +22,18 @@ for ($i = 0; $i -lt $AudioDevices.Count; $i++) {
     Write-Host "$($i + 1): $($AudioDevices[$i].Name)"
 }
 
-# Benutzerauswahl
-$Selection = Read-Host "Please select the number of the audio device to be used: "
-
-# Select audio device
-if ($Selection -match "^\d+$" -and [int]$Selection -le $AudioDevices.Count) {
-    $SelectedDevice = $AudioDevices[[int]$Selection - 1]
-    Write-Host "Selected Device: $($SelectedDevice.Name)"
-    $SelectedDevice | Set-AudioDevice
-	Start-Pause 4
+# User selection of the audio device
+While ( $true ) {
+    $Selection = Read-Host "Please select the number of the audio device to be used"
+	
+    if ($Selection -match "^\d+$" -and [int]$Selection -le $AudioDevices.Count -and [int]$Selection -gt 0 ) {
+        $SelectedDevice = $AudioDevices[[int]$Selection - 1]
+        Write-Host "Selected Device: $($SelectedDevice.Name)"
+        $SelectedDevice | Set-AudioDevice	
+	    Start-Sleep -Seconds  3
+	    exit
     } else {
-    Write-Host "Invalid selection, please try again!"
+        Write-Host "Invalid selection, please try again!"
+	    Start-Sleep -Seconds 1  
+	}
 }
